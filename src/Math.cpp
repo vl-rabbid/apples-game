0fe4ce38f76f92be9f16e6462b1a18c0fc4545f9
vl-rabbid <vlad.solodovnikov1999@gmail.com>
2026-08-25 14:01:04 +1000

    refactor: migrate build system to CMake for cross-platform support

---
+++ src/Math.cpp
@@ -0,0 +1,121 @@
+#include "Math.h"
+#include <cstdlib>
+#include <SFML/Graphics.hpp>
+#include <string>
+#include <cmath>
+
+namespace ApplesGame
+{
+	float GetRandomFloat(float minValue, float maxValue)
+	{
+		return minValue + (rand() / (float)RAND_MAX) * (maxValue - minValue);
+	}
+
+	int GetRandomInt(int minValue, int maxValue)
+	{
+		return minValue + rand() % (maxValue - minValue + 1);
+	}
+
+	Position2D GetRandomPositionInScreen(float screenWidth, float screenHeight)
+	{
+		Position2D result;
+		result.x = GetRandomFloat(0, screenWidth);
+		result.y = GetRandomFloat(0, screenHeight);
+		return result;
+	}
+
+	bool IsRectanglesCollide(Position2D rect1Position, Vector2D rect1Size, Position2D rect2Position, Vector2D rect2Size)
+	{
+		float dx = std::fabs(rect1Position.x - rect2Position.x);
+		float dy = std::fabs(rect1Position.y - rect2Position.y);
+		return (dx <= (rect1Size.x + rect2Size.x) / 2.f && dy <= (rect1Size.y + rect2Size.y) / 2.f);
+	}
+
+	bool IsCirclesCollide(Position2D circ1Position, float circ1Radius, Position2D circ2Position, float circ2Radius)
+	{
+		float distanceSqr = (circ1Position.x - circ2Position.x) * (circ1Position.x - circ2Position.x)
+			+ (circ1Position.y - circ2Position.y) * (circ1Position.y - circ2Position.y);
+		return (distanceSqr <= (circ1Radius + circ2Radius) * (circ1Radius + circ2Radius));
+	}
+
+	void SetSpriteSize(sf::Sprite& sprite, float desiredWidth, float desiredHeight)
+	{
+		sf::FloatRect spriteRect = sprite.getLocalBounds();
+		sf::Vector2f scale = { desiredWidth / spriteRect.width, desiredHeight / spriteRect.height};
+		sprite.setScale(scale);
+	}
+
+	void SetSpriteRelativeOrigin(sf::Sprite& sprite, float originX, float originY)
+	{
+		sf::FloatRect spriteRect = sprite.getLocalBounds();
+		sf::Vector2f origin = { spriteRect.width * originX, spriteRect.height * originY };
+		sprite.setOrigin(origin);
+	}
+
+	void SetTextRelativeOrigin(sf::Text& text, float originX, float originY)
+	{
+		sf::FloatRect spriteRect = text.getLocalBounds();
+		sf::Vector2f origin = { spriteRect.width * originX, spriteRect.height * originY };
+		text.setOrigin(origin);
+	}
+
+	Vector2D GetTextRelativeOrigin(sf::Text& text)
+	{
+		sf::FloatRect spriteRect = text.getLocalBounds();
+		sf::Vector2f origin = text.getOrigin();
+		Vector2D relativeOrigin;
+		relativeOrigin.x = origin.x / spriteRect.width;
+		relativeOrigin.y = origin.y / spriteRect.height;
+		return relativeOrigin;
+	}
+
+	void SetTextScreenRelativePosition(sf::Text& text, float screenWidth, float screenHeight, float relativePositionX, float relativePositionY)
+	{
+		sf::Vector2f position = { screenWidth * relativePositionX, screenHeight * relativePositionY };
+		text.setPosition(position);
+	}
+
+	Vector2D GetTextScreenRelativePosition(sf::Text& text, float screenWidth, float screenHeight)
+	{
+		sf::Vector2f position = text.getPosition();
+		Vector2D relativePosition;
+		relativePosition.x = position.x / screenWidth;
+		relativePosition.y = position.y / screenHeight;
+		return relativePosition;
+	}
+
+	void ShiftTextPozition(sf::Text& text, float shiftX, float shiftY)
+	{
+		sf::Vector2f position = text.getPosition();
+		position.x += shiftX;
+		position.y += shiftY;
+		text.setPosition(position);
+	}
+
+	bool rollChance(float percent)
+	{
+		return GetRandomFloat(0, 100) <= percent;
+	}
+
+	std::string BoolToString(const bool& flag)
+	{
+		if (flag)
+		{
+			return "Yes";
+		}
+		return "No";
+	}
+
+	void SortLeaderboardDesc(std::vector<std::pair<std::string, int>>& leaderboard)
+	{
+		for (unsigned int i = 1; i < leaderboard.size(); ++i)
+		{
+			unsigned int j = i;
+			while (j > 0 && leaderboard[j - 1].second < leaderboard[j].second)
+			{
+				std::swap(leaderboard[j - 1], leaderboard[j]);
+				--j;
+			}
+		}
+	}
+}
